51759bbad734742717f65cb5d81fc13becf60449
Ben Newman <benjamin.newman@uon.edu.au>
2019-05-02 00:50:25 +0000

    working

--- task.c
+++ task.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+void swap(int *x, int*y);
+
+int main()
+{
+    int a=1, b=2;
+    swap(&a, &b);
+    printf("a is %d, b is %d\n", a, b);
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+    
+    temp = *x;
+    
+    *x=*y;
+    *y=temp;
+}
